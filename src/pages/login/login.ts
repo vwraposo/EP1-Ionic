11b3a98ff1eb0401aff63403f65b282fa4954b76
João Daniel <jotaf.daniel@gmail.com>
2017-05-12 12:31:52 -0300

    sei lá, são 12h30 de sexta

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,11 +3,14 @@ import { MenuController, ToastController, ViewController, NavController, NavPara
 import { Storage } from '@ionic/storage';
 
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { CurrentUser } from '../../providers/current-user'
+import { CurrentUser } from '../../providers/current-user';
 
-import { Signup } from '../signup/signup'
-import { User } from '../../models/user' 
-import { SeminarList } from '../seminar-list/seminar-list' 
+import { Signup } from '../signup/signup';
+import { User } from '../../models/user';
+import { SeminarList } from '../seminar-list/seminar-list';
+
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 
 /**
@@ -33,7 +36,9 @@ export class Login {
     public currUser: CurrentUser, 
     public toastCtrl: ToastController, 
     private menu: MenuController,
-    private storage: Storage) {
+    private storage: Storage,
+    private http: Http) {
+
     this.menu.enable(false, 'side_menu');
 
     // Se ja logado
@@ -71,32 +76,71 @@ export class Login {
   }
 
   save () {
-    if (this.login.value.login != '' && 
+    if (
+      this.login.value.login    != '' && 
       this.login.value.password != '' && 
-      this.login.value.u_type != '') {
+      this.login.value.u_type   != '') {
 
-      this.user = new User (this.login.value.login,
-        "Nome Hardcoded", this.login.value.u_type == "S");
+      let name = 'HARDCODED UNTIL REQUEST';
+      let nusp = this.login.value.login;
+      let pass = this.login.value.password;
+      let stud = this.login.value.u_type == "S";
+
+      let url = 'http://207.38.82.139:8001/login/';
+
+      url = url + (stud ? "student" : "teacher");
+
+      console.log("Target: "+url);
+
+      let myHeaders = new Headers();
+      myHeaders.append('content-type','application/json');
+      myHeaders.append('Access-Control-Allow-Origin','*');
+
+      let options = new RequestOptions({headers: myHeaders});
+
+      let data = JSON.stringify({
+        name: name,
+        nusp: nusp,
+        pass: pass
+      });
+
+      this.http.post(url,data,options).map(res => res.json()).subscribe(
+        data => {
+            console.log('D: '+data);
+        },
+        err => {
+        console.log('E: '+Object.keys(err));
+            console.log('body: '+err._body);
+            console.log('status: '+err.status);
+            console.log('ok: '+err.ok);
+            console.log('statusText: '+err.statusText);
+            console.log('headers: '+err.headers._headers);
+            console.log('type: '+err.type);
+            console.log('url: '+err.url);
+        });
+
+        //this.user = new User (this.login.value.login,
+        //"Nome Hardcoded", this.login.value.u_type == "S");
 
       // Salva no Storage
-      this.storage.set('user_login', this.user.nusp);
-      this.storage.set('user_type', this.user.is_student);
+      //this.storage.set('user_login', this.user.nusp);
+      //this.storage.set('user_type', this.user.is_student);
 
       // Usuario atual
-      this.currUser.setUser(this.user);
-      console.log (this.user);
+      //this.currUser.setUser(this.user);
+      //console.log (this.user);
 
 
-      this.navCtrl.push(SeminarList).
-        then(() => {
-          const index = this.viewCtrl.index;
-          this.navCtrl.remove(index);
-        });
+      //this.navCtrl.push(SeminarList).
+      //then(() => {
+      //const index = this.viewCtrl.index;
+      //this.navCtrl.remove(index);
+      //});
 
     } else {
       console.log ("invalid input - missing information");
       let toast = this.toastCtrl.create({
-        message: 'Log in failed',
+        message: 'Log in failed - missing information',
         duration: 3000
       });
       toast.present();
