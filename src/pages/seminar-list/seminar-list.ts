34aa1537c0aeb5ca0c964497ff09c5e40fde2ffd
João Daniel <jotaf.daniel@gmail.com>
2017-05-12 23:53:28 -0300

    o post de new seminar tá devolvendo true, mas não adiciona de fato (??)

--- src/pages/seminar-list/seminar-list.ts
+++ src/pages/seminar-list/seminar-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MenuController, ToastController, AlertController, NavController, NavParams } from 'ionic-angular';
+import { MenuController, ViewController, ToastController, AlertController, NavController, NavParams } from 'ionic-angular';
 
 import { SeminarPage } from '../seminar-page/seminar-page'
 import { Seminar } from '../../models/seminar'
@@ -8,7 +8,7 @@ import { User } from '../../models/user'
 import { CurrentUser } from '../../providers/current-user'
 
 import 'rxjs/add/operator/map';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'page-seminar-list',
@@ -22,6 +22,7 @@ export class SeminarList {
 
   constructor(
     public navCtrl: NavController,
+    public viewCtrl: ViewController,
     public navParams: NavParams, 
     public currUser: CurrentUser,
     public alertCtrl: AlertController,
@@ -73,7 +74,45 @@ export class SeminarList {
           text: 'Save',
           handler: data => {
             console.log('New Seminar: ' + data.name );
-            this.seminars.push(new Seminar(22, "" + data.name));
+            let myHeaders = new Headers();
+            myHeaders.append('Content-Type','application/json');
+
+            let myOpts = new RequestOptions({headers: myHeaders});
+
+            let myBody = JSON.stringify({
+                name: data.name
+            });
+
+            this.http.post('http://207.38.82.139:8001/seminar/add',myBody,myOpts)
+                .map(res => res.json()).subscribe(
+                data => {
+                    console.log('REQUEST DATA: '+data.success);
+                    if (data.success) {
+                        console.log('new seminar ok, updating');
+                        this.toastCtrl.create({
+                            message: "Seminar successfully created!",
+                            duration:3000
+                        }).present();
+                        this.navCtrl.push(SeminarList).
+                            then(() => {
+                                const index = this.viewCtrl.index;
+                                this.navCtrl.remove(index);
+                            });
+                    } else {
+                        console.log('request returned false');
+                        this.toastCtrl.create({
+                            message: data.success,
+                            duration: 3000
+                        }).present();
+                    }
+                },
+                err => {
+                    console.log('request failed');
+                    this.toastCtrl.create({
+                        message: "Request failed - check your connection",
+                        duration: 3000
+                    }).present();
+                });
           }
         }
       ]
